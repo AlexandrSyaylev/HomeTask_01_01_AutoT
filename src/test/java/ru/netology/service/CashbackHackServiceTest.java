a36e76d03132af5da50b9ac8caf2040af8e8ea35
Alexander Syayalev <alexxs@inbox.ru>
2022-03-20 23:36:49 +0300

    add CashbackHackService.java class, add TestClass TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateHackNormal(){
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(500,service.remain(1500));
+    }
+
+    @Test
+    public void shouldCalculateHackMinBorder(){
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(999,service.remain(1001));
+    }
+
+    @Test
+    public void shouldCalculateNearMinBorder(){
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(998,service.remain(1002));
+    }
+
+    @Test
+    public void shouldCalculateUpperBoard(){
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(1,service.remain(1999));
+    }
+
+    @Test
+    public void shouldCalculateNearUpperBoard(){
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(2,service.remain(1998));
+    }
+
+    @Test
+    public void shouldCalculateBorder(){  //here we have Error
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(0,service.remain(2000));
+    }
+}
