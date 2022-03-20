81536c0964615c77abdfeb34cc152c3a0ea29c5c
Alexander Syayalev <alexxs@inbox.ru>
2022-03-20 23:46:47 +0300

    add TestNG tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,38 +6,55 @@ import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     @Test
-    public void shouldCalculateHackNormal(){
+    public void shouldCalculateHackNormal() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(500,service.remain(1500));
+        assertEquals(500, service.remain(1500));
     }
 
     @Test
-    public void shouldCalculateHackMinBorder(){
+    public void shouldCalculateHackMinBorder() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(999,service.remain(1001));
+        assertEquals(999, service.remain(1001));
     }
 
     @Test
-    public void shouldCalculateNearMinBorder(){
+    public void shouldCalculateNearMinBorder() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(998,service.remain(1002));
+        assertEquals(998, service.remain(1002));
+    }
+    @Test
+    public void shouldCalculateUpperBoard() {
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(1, service.remain(1999));
     }
 
     @Test
-    public void shouldCalculateUpperBoard(){
+    public void shouldCalculateNearUpperBoard() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(1,service.remain(1999));
+        assertEquals(2, service.remain(1998));
     }
 
     @Test
-    public void shouldCalculateNearUpperBoard(){
+    public void shouldCalculateBorder() {  //here we have Error
         CashbackHackService service = new CashbackHackService();
-        assertEquals(2,service.remain(1998));
+        assertEquals(0, service.remain(1000));
     }
 
     @Test
-    public void shouldCalculateBorder(){  //here we have Error
+    public void shouldCalculateUpperBorder() {  //here we have Error
         CashbackHackService service = new CashbackHackService();
-        assertEquals(0,service.remain(2000));
+        assertEquals(0, service.remain(2000));
+    }
+
+    @Test
+    public void shouldCalculatMultiplyBoundary() {  //here we have Error
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(0, service.remain(4000));
+    }
+
+    @Test
+    public void shouldCalculateZero() {  //here we have Error
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(0, service.remain(0));
     }
 }
