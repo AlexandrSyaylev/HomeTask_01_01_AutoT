c0b0427b3469596dd490341333faad2103adde19
Alexander Syayalev <alexxs@inbox.ru>
2022-03-25 23:00:39 +0300

    Falling test

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,10 +4,13 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+    /*public int remain(int amount) {
 
         if((amount % boundary) != 0) {
             return boundary - amount % boundary;
             }
         else return 0;
-        }
+        }*/
 }
